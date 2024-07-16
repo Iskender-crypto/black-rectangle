35be01a96815236961ba0b100b0f1541f7d7f4ce
iskender-crypto <iskenderazamov@gmail.com>
2024-07-16 10:14:40 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+<template>
+  <div class="black-rectangle"></div>
+</template>
+
+<style>
+body{
+  height: 100dvh;
+  display: flex;
+  align-items: center;
+}
+.black-rectangle{
+  height: 100px;
+  width: 100px;
+  background-color: black;
+}
+</style>
